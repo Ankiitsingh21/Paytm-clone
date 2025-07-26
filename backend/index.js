7f78e0a0434a448bb0b210343e7d21bcaa337ac7
ankit singh <ankiitsingh21@gmail.com>
2025-07-26 17:33:35 +0530

    Added routing logic and setup mongodb and created a express server

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,19 @@
 const express = require("express");
+const cors = require('cors');
+const apiRoutes= require("./routes/index.js");
+const PORT = 3000;
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use("/api",apiRoutes);
+
+app.listen(PORT,async()=>{
+        console.log(`Server started at ${PORT}`)
+});
+
 
 
---
+++ backend/routes/v1/index.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+
+
+router.post("/signUp",);
+router.post("signIn",);
+router.put("/update",);
+
+module.exports = router;

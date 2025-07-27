192fa38f44f535473e3f8d30c3bb04529d320fa5
ankit singh <ankiitsingh21@gmail.com>
2025-07-27 18:29:11 +0530

    added routes fro transfer logic and increment and decrement logic

--- backend/routes/v1/index.js
+++ backend/routes/v1/index.js
@@ -4,6 +4,7 @@ const jwt= require("jsonwebtoken")
 const User = require("../../repository/user");
 const { JWT } = require("../../config/serverConfig");
 const verifyToken = require("../../middleware/auth");
+const Account = require("../../repository/account");
 // const { verifyToken } = require("../../middleware/auth");
 const router = express.Router();
 
@@ -38,7 +39,9 @@ router.post("/signUp",async(req,res)=>{
         const token =jwt.sign({
                 userId:db._id
         },JWT);
-        
+        // console.log(db._id);
+        const amount = req.body.amount;
+       const amm = await Account.create({ id: db._id, balance: amount });
         return res.status(201).json({
                 message:"Successfully signup",
                 token:token
@@ -76,7 +79,8 @@ router.post("/signIn",async(req,res)=>{
         }
 });
 router.put("/update",verifyToken,async(req,res)=>{
-        // const token =
+        try {
+                 // const token =
         const user = await User.findByIdAndUpdate(req.userId,req.body, { new: true });
         // console.log(user);
         // console.log("hi");
@@ -84,6 +88,72 @@ router.put("/update",verifyToken,async(req,res)=>{
                 message:"Updated Successfully",
                 data:user
         })
+        } catch (error) {
+               return res.status(401).json({
+                message:"Failed to update",
+                error:error
+               }) 
+        }
 });
+
+router.post("/transfer",verifyToken,async(req,res)=>{
+        const fromAccountId = req.userId;
+        const userName = req.body.username;
+        const toAccountId = await User.findOne({username:userName});
+        // console.log(toAccountId, {userName});
+        if (!toAccountId) {
+        return res.status(404).json({ message: "Receiver not found" });
+        }
+        const amount = req.body.amount;
+        const Balance = await checkBalance(fromAccountId);
+        // console.log({Balance});
+        if(amount>Balance){
+                return res.status(201).json({
+                        message:"The amount you entered is above the balance",
+                        amount:amount
+                })
+        }
+        // console.log(fromAccountId," ",toAccountId._id);
+        //decrement from the sending user
+        const decrement = await Decrement(fromAccountId,amount);
+        const increment = await Increment(toAccountId._id,amount);
+        if(decrement === true && increment === true){
+                return res.status(201).json({
+                message:"Money Transfered Succesfully",
+                from:fromAccountId,
+                to:toAccountId._id,
+                amount:`₹${amount}`
+        });
+        }
+        return res.status(401).json({
+                message:"Unable to update BAlance"
+        })
+})
+
+
+async function  checkBalance(id){
+       const account = await Account.findOne({ id: id });
+//        console.log(account);
+       return account?.balance; 
+}
+
+async function Decrement(id, amount) {
+    const account = await Account.findOneAndUpdate(
+        { id },
+        { $inc: { balance: -amount } },
+        { new: true }
+    );
+    return !!account; // true if account exists
+}
+
+async function Increment(id, amount) {
+    const account = await Account.findOneAndUpdate(
+        { id },
+        { $inc: { balance: amount } },
+        { new: true }
+    );
+    return !!account;
+}
+
 
 module.exports = router;
